9addb3b1afefa9aa517691abc1fa8b770e4848fd
MatheoRigolo <119617440+MatheoRigolo@users.noreply.github.com>
2022-12-02 06:18:13 +0100

    ça marche vraiment

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const Discord = require('discord.js');
-var http = require('http');
 var XMLHttpRequest = require('xhr2');
+var dotenv = require('dotenv');
+dotenv.config();
 
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent], partials: [Partials.Channel] });
-
-const token = 'MTA0Nzk0MDM1Njg1NzE1NTYzNA.GocpkA.bOvk1eR90neKLyfLlrJ3QVqJI4YsT5C35BQt7A'
 
 client.once('ready', () => {
   console.log('ready pour les polypotes');
 });
 
-client.login(token);
+client.login();
 
 client.on("messageCreate", (message) => {
   if (message.author.bot == false && message.content == 'change le thème') {
